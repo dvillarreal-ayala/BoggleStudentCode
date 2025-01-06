881d020ee17ab7ed57bbfb120a8bc16c7b262e60
damianayala <damian.villarreal-ayala@menloschool.org>
2025-01-06 13:43:43 -0800

    1st commit; added notes & ideas in comments

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |  Time  |                                                                                                                        Update |
+|:--------|:------:|------------------------------------------------------------------------------------------------------------------------------:|
+| April 5 | 9-10pm |                                                                                    I tried to write abstract, efficient code. |
+| Jan 6   | 1-2pm  | Started problem set. Written down ideas on how to start, including using BFS, DFS, and TSTs. Need to begin implementing code. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Boggle.java
+++ src/Boggle.java
@@ -2,15 +2,42 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Boggle {
-
+    /**
+     * - Letters CAN"T be used more than once in a word.
+     * - No diagonal letters, only cardinal directions can be used
+     *
+     * Ideas?
+     * - perhaps incorporate recursion?
+     * - Incorporate TST or Trie
+     * -> Dictionary and "good words" TSTs
+     * - use bfs?
+     * -> each path would need a "travel log" keeping track of places it's been on the board
+     *
+     * - use dfs?
+     * -> parent node structure so that we can traverse in reverse using the parental nodes.
+     * -> Go all the way down, and if it's a deadend, go back and check if the other available options would form a word.
+     *
+     * - Is it a good idea to remove a "good word" after it has been found in the board?
+     *
+     * I need some way to know when to stop; use a while loop?
+     */
     public static String[] findWords(char[][] board, String[] dictionary) {
-
-        ArrayList<String> goodWords = new ArrayList<String>();
-
         // TODO: Complete the function findWords(). Add all words that are found both on the board
         //  and in the dictionary.
+        //First char of the first word in the dictionary
+        char currentChar = dictionary[0].charAt(0);
 
-        // Convert the list into a sorted array of strings, then return the array.
+        //Array list of dictionary words found in board.
+        ArrayList<String> goodWords = new ArrayList<String>();
+
+        //Begin to
+
+
+
+
+
+
+        // Convert the list into a sorted array of strings of all dictionary words found in the board, then return the array.
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
