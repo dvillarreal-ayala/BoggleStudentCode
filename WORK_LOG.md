b8fa0806af43b056dba9f7474437f5033840397f
damianayala <damian.villarreal-ayala@menloschool.org>
2025-01-10 09:58:03 -0800

    3rd commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -5,6 +5,7 @@ Name: YOUR NAME HERE
 | April 5 | 9-10pm  |                                                                                    I tried to write abstract, efficient code. |
 | Jan 6   |  1-2pm  | Started problem set. Written down ideas on how to start, including using BFS, DFS, and TSTs. Need to begin implementing code. |
 | Jan 8   | 11-12pm |                        We went over possible implementations of DFS for the project. Put in the version of DFS shown in class |
+|         |         |                                                                                                                               |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Boggle.java
+++ src/Boggle.java
@@ -7,7 +7,7 @@ public class Boggle {
      * - No diagonal letters, only cardinal directions can be used
      *
      * Ideas?
-     * - perhaps incorporate recursion?
+     * - incorporate recursion?
      * - Incorporate TST or Trie
      * -> Dictionary and "good words" TSTs
      * - use bfs?
@@ -31,8 +31,15 @@ public class Boggle {
         //Array list of dictionary words found in board.
         ArrayList<String> goodWords = new ArrayList<String>();
 
-        //Begin to
-
+        TST johnMilton = new TST();
+        //Subsection: Go down through the board until we find every word.
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                dfs(board, i, j);
+            }
+        }
 
 
 
@@ -44,44 +51,58 @@ public class Boggle {
     }
 
     // possible DFS implementation
-    public void dfs(char[][] grid, int col, int row)
+    public static void dfs(char[][] grid, int row, int col)
     {
         //out of bounds base case, making sure we stay inside grid.
-        if (col < 0 || row < 0 || col >= grid.length || row >= grid[0].length)
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length)
         {
             return;
         }
         //base case; checking if we've visited this square before
-        if (grid[col][row] == '0')
+        if (grid[row][col] == '0')
         {
             return;
         }
         //base case for invalid prefix?
 
+        /**
+         * Step 1: Build words and append along the way
+         * Step 2: Check if findWords is working by having it print things out
+         * Step 3:
+          */
+        char testCaseWordThing = '';
         //If it isn't caught in the two base cases, mark square as visited.
-        grid[col][row] = '0';
+        char placeholder = grid[row][col];
+        grid[row][col] = '0';
 
         //recurse
-        dfs(grid, col - 1, row);
-        dfs(grid, col + 1, row);
-        dfs(grid, col, row - 1);
-        dfs(grid, col, row + 1);
+        //Modify dfs so that whenever we're recursing, we're appending to the word
+        // so that each successive call the word gets bigger.
+        dfs(grid, row - 1, col);
+        dfs(grid, row + 1, col);
+        dfs(grid, row, col - 1);
+        dfs(grid, row, col + 1);
+
+        // mark this square as NOT visited
+        grid[row][col] = placeholder;
     }
 
-    public int numIslands(char[][] grid)
-    {
-        int count = 0;
-
-        for(int i = 0; i < grid.length; i++)
-        {
-            for(int j = 0; j < grid[0].length; j++)
-            {
-                if (grid[i][j] == '1')
-                {
-                    dfs(grid, i, j);
-                    count++;
-                }
-            }
-        }
-    }
+//    public int numIslands(char[][] grid)
+//    {
+//        int count = 0;
+//
+//        for(int i = 0; i < grid.length; i++)
+//        {
+//            for(int j = 0; j < grid[0].length; j++)
+//            {
+//                if (grid[i][j] == '1')
+//                {
+//                    dfs(grid, i, j);
+//                    count++;
+//                }
+//            }
+//        }
+//
+//        return count;
+//    }
 }
